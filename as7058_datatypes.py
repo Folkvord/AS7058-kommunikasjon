a6951c4bb63de377aa365056debcc58fa81c75c1
FOLKVORD <folkvord.kristoffer@gmail.com>
2025-04-11 13:12:35 +0200

    Array type

--- as7058_datatypes.py
+++ as7058_datatypes.py
@@ -8,5 +8,10 @@ def uint24(value: int) -> bytes:
     return int.to_bytes(value, m.UINT24, byteorder="little")
 def uint32(value: int) -> bytes:
     return int.to_bytes(value, m.UINT32, byteorder="little")
+def array_type(array: list) -> bytes:
+    return_type = bytes([])
+    for byte in array:
+        return_type += uint8(byte)
+    return return_type
 def custom_type(value: int, size: int) -> bytes:
     return value.to_bytes(size, byteorder="little")
